ada3674e4e427a3200045cfbb2c5837a55d8d745
EyalPazz <eyalp700@gmail.com>
2024-07-09 11:01:01 +0300

    fix(test): adjust test to runner

--- tests/manipulations_test.go
+++ tests/manipulations_test.go
@@ -2,7 +2,8 @@ package tests
 
 import (
 	"promoter/helpers/manipulations"
-
+    "os"
+    "path"
 	"testing"
 )
 
@@ -21,7 +22,8 @@ func TestFileExists(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 
-			exists := manipulations.FileExists(tt.filePath)
+            home, _ := os.UserHomeDir()
+			exists := manipulations.FileExists(path.Join(home, tt.filePath))
 
 			if tt.expectedValue != exists {
 				t.Errorf("expected %v but got %v", tt.expectedValue, exists)
