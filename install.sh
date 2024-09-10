623a2951e35b8b4147b0a79400e94623f4f03803
EyalPazz <eyalp700@gmail.com>
2024-09-10 14:57:03 +0300

    build: add support for more platforms

--- install.sh
+++ install.sh
@@ -1,7 +1,39 @@
 #!/bin/bash
 
-# Define the download URL
-URL="https://github.com/EyalPazz/promoter/releases/download/v0.1.14/promoter"
+# Detect the platform and architecture
+OS=$(uname -s)
+ARCH=$(uname -m)
+VERSION=0.2.0
+
+# Map platform and architecture to the correct binary name
+case "$OS" in
+  Linux)
+    PLATFORM="linux"
+    ;;
+  Darwin)
+    PLATFORM="darwin"
+    ;;
+  *)
+    echo "Unsupported OS: $OS"
+    exit 1
+    ;;
+esac
+
+case "$ARCH" in
+  x86_64)
+    ARCH="amd64"
+    ;;
+  arm64)
+    ARCH="arm64"
+    ;;
+  *)
+    echo "Unsupported architecture: $ARCH"
+    exit 1
+    ;;
+esac
+
+# Define the download URL based on platform and architecture
+URL="https://github.com/EyalPazz/promoter/releases/download/${VERSION}/promoter_${PLATFORM}_${ARCH}"
 
 # Define the install path
 INSTALL_PATH="/usr/local/bin/promoter"
@@ -16,4 +48,3 @@ chmod +x promoter
 sudo mv promoter $INSTALL_PATH
 
 echo "promoter installed successfully!"
-
