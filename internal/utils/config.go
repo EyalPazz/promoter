60f8c9d7b3faee579106764cb5d3d93cf4978874
EyalPazz <eyalp700@gmail.com>
2024-11-26 11:31:40 +0200

    chore: fmt

--- internal/utils/config.go
+++ internal/utils/config.go
@@ -18,25 +18,25 @@ func GetImageTagKey() string {
 }
 
 func ValidateProjectAttributes(project, region string) (string, string, error) {
-    project = firstNonEmpty(project, viper.GetString("project-name"))
-    region = firstNonEmpty(region, viper.GetString("region"))
+	project = firstNonEmpty(project, viper.GetString("project-name"))
+	region = firstNonEmpty(region, viper.GetString("region"))
 
-    // Validate required fields
-    if project == "" {
-        return "", "", fmt.Errorf("project name must be specified either as a flag or in the config file")
-    }
-    if region == "" {
-        return "", "", fmt.Errorf("region must be specified either as a flag or in the config file")
-    }
+	// Validate required fields
+	if project == "" {
+		return "", "", fmt.Errorf("project name must be specified either as a flag or in the config file")
+	}
+	if region == "" {
+		return "", "", fmt.Errorf("region must be specified either as a flag or in the config file")
+	}
 
-    return project, region, nil
+	return project, region, nil
 }
 
 func firstNonEmpty(values ...string) string {
-    for _, value := range values {
-        if value != "" {
-            return value
-        }
-    }
-    return ""
+	for _, value := range values {
+		if value != "" {
+			return value
+		}
+	}
+	return ""
 }
