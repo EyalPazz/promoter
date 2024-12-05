EyalPazz/promoter

commit a6ead83897e57180e40be452a72134274e8f9bc3

fix: this conditional should be here

internal/manipulations/file.go
@@ -20,16 +20,17 @@ func ChangeServiceTag(project, service, env, tag string) (bool, error) {
 
 	imageTagKey := utils.GetImageTagKey()
 
-	if app[imageTagKey] == tag {
-		fmt.Printf("Service %s is already at latest tag \n", service)
-		return false, nil
-	}
-
 	if _, ok := app[imageTagKey]; ok {
 		app[imageTagKey] = tag
 	} else {
 		return false, errors.New("image tag not found in the service's fields")
 	}
+
+	if app[imageTagKey] == tag {
+		fmt.Printf("Service %s is already at latest tag \n", service)
+		return false, nil
+	}
+
 
 	if err = utils.WriteToProjectFile(project, env, config); err != nil {
 		return false, err

commit 119204d5d8686f7f83173b43ac39f3a9bbed690a

fix: i broke it in a6ead83

internal/manipulations/file.go
@@ -22,14 +22,12 @@ func ChangeServiceTag(project, service, env, tag string, interactive bool) (bool
 
 	imageTagKey := utils.GetImageTagKey()
 
-	if _, ok := app[imageTagKey]; ok {
-		app[imageTagKey] = tag
-	} else {
+	if _, ok := app[imageTagKey]; !ok {
 		return false, errors.New("image tag not found in the service's fields")
-	}
+	} 
 
 	if app[imageTagKey] == tag {
-		fmt.Printf("Service %s is already at latest tag \n", service)
+		fmt.Printf("Service %s is already at latest / input tag \n", service)
 		return false, nil
 	}
 
@@ -52,6 +50,7 @@ func ChangeServiceTag(project, service, env, tag string, interactive bool) (bool
 		return false, nil
 	}
 
+    app[imageTagKey] = tag
 	if err = utils.WriteToProjectFile(project, env, config); err != nil {
 		return false, err
 	}