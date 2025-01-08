f52c2b3b160445c6cf9d9b2ae6c464952fdc8ac1
Eyal Paz <eyalp700@gmail.com>
2025-01-08 12:34:10 +0200

    fix: refresh manifest repo before  cmd

--- cmd/commands/get_services.go
+++ cmd/commands/get_services.go
@@ -11,10 +11,16 @@ func GetServicesCmd(cmd *cobra.Command) {
 
 	env, _ := cmd.Flags().GetString("env")
 	project, _ := cmd.Flags().GetString("project")
+	passphrase, _ := cmd.Flags().GetBool("passphrase")
 
 	project, _, err := utils.ValidateProjectAttributes(project, "")
 
-	if project == "" {
+	if err != nil {
+		fmt.Print(err)
+		return
+	}
+
+	if err = utils.RefreshRepo(passphrase); err != nil {
 		fmt.Print(err)
 		return
 	}
